8a2e6ff90890194fa8cfe96519ee16ce188f37c4
andsaru <andsaru@hotmail.es>
2021-07-08 08:08:51 +0200

    Creacion de las rutas

---
+++ src/components/config/context/hooks/pages/routers/Router.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Login from "../pages/Login";
+import Public from "../pages/Public";
+import Dashboard from "../pages/Dashboard";
+import Admin from "../pages/Admin";
+import PrivateRoute from "../pages/PrivateRoute";
+
+import Navbar from "../components/Navbar";
+
+export default function Router() {
+    
+    return (
+        <BrowserRouter>
+
+          <Switch>
+            <Route exact path="/" component={Login}/>
+            <div>
+              <Navbar />
+              
+              <Route path="/public" component={Public} />
+              <PrivateRoute path='/dashboard'><Dashboard /></PrivateRoute> {/* Only for logged in users */}
+              <PrivateRoute path='/admin'><Admin /></PrivateRoute> {/* Only for logged in users who are admins */}
+              <Route><h3 className="text-center pt-3">Esta página no existe</h3></Route>
+              </div>  
+          </Switch>
+        </BrowserRouter>
+    )
+}
