87afaf8fa90ea5b9036d47a8ebd47c6fe6448bc9
andsaru <andsaru@hotmail.es>
2021-07-08 08:05:04 +0200

    Creacion de globales para conectarlas al servidor

---
+++ src/components/config/config.js
@@ -0,0 +1,19 @@
+/**
+ * Constantes globales que son susceptibles de cambiar
+ * en el futuro y que queremos tener localizadas.
+ */
+
+ const PORT = 8000;
+ const BASE_API_URL = `http://localhost:${PORT}/api/`;
+ const LOGIN_URL = BASE_API_URL + "login_check";
+ const SESSION_URL = BASE_API_URL + "session";
+ const PRIVATE_URL = BASE_API_URL + "private";
+ const ADMIN_URL = BASE_API_URL + "admin";
+ 
+ export {
+     BASE_API_URL,
+     LOGIN_URL,
+     SESSION_URL,
+     PRIVATE_URL,
+     ADMIN_URL
+ };
