9d1524af409dda07790ba4d2566c844ca963ed60
andsaru <andsaru@hotmail.es>
2021-07-13 12:57:53 +0200

    prueba commit

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -31,13 +31,6 @@ export default function Admin() {
     return (
 
         <div className="container text-center pt-5">
-            {/* <h3>Email: {admin_users.email} </h3>
-            <h3>Código: {admin_users.first_name} </h3>
-            <h3>Nombre: {admin_users.last_name}</h3>
-            <h3>Contraseña: {admin_users.password}</h3>
-            <h3>Teléfono: {admin_users.phone}</h3>
-            <h3>Turno: {admin_users.class_shift}</h3>
-            <h3>Horas: {admin_users.shift_duration}</h3> */}
 
             <div className="row">
                 {admin_users.map((admin_users) => {
@@ -47,9 +40,7 @@ export default function Admin() {
                             <li className="list-group-item">{admin_users.email}</li>
                             <li className="list-group-item">{admin_users.first_name}</li>
                             <li className="list-group-item">{admin_users.last_name}</li>
-                            
-                            <li className="list-group-item">{admin_users.phone}</li>
-                            
+                            <li className="list-group-item">{admin_users.phone}</li>            
                             <li className="list-group-item">{admin_users.class_shift}, {admin_users.shift_duration}</li>
                             <li className="list-group-item">
                            <button className="btn btn-warning" /* onClick={removeContact(admin_user.id)} */>Eliminar</button>
